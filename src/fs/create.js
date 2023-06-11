b29637d625fc0bf10b150762a0858f5c0bb64785
Olga Chernega <eliang.cheng@gmail.com>
2023-06-11 20:19:55 +0300

    feat: add fs module solutions

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const destination = path.join(__dirname, "files", "fresh.txt");
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await fs.appendFile(destination, "I am fresh and young", { flag: "ax" });
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await create();
