31963e565308d626e4730c8ca443a90203d7f432
Olga Chernega <eliang.cheng@gmail.com>
2023-06-12 20:23:21 +0300

    feat: add streams module solutions

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,21 @@
+// terminal command: node src/streams/write
+// write smth in the terminal, the output will be in the file fileToWrite.txt
+
+import fs from "fs";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const destination = path.join(__dirname, "files", "fileToWrite.txt");
+
 const write = async () => {
-    // Write your code here 
+  try {
+    const writeStream = fs.createWriteStream(destination);
+    process.stdin.pipe(writeStream);
+  } catch (error) {
+    throw new Error("Write stream operation failed");
+  }
 };
 
 await write();
